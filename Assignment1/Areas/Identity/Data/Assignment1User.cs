5f494df683871842735926b7ef6ea64f0a6f23eb
lufurt <lufurt@yahoo.com.br>
2023-03-01 18:55:19 -0500

    login and register

---
+++ Assignment1/Areas/Identity/Data/Assignment1User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Assignment1.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Assignment1User class
+public class Assignment1User : IdentityUser
+{
+}
+
--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Assignment1.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Assignment1ContextConnection") ?? throw new InvalidOperationException("Connection string 'Assignment1ContextConnection' not found.");
+
+builder.Services.AddDbContext<Assignment1Context>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<Assignment1User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<Assignment1Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -13,11 +23,12 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
